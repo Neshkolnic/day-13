509d2b1823be38bde5ef2b67c384b305731c0de5
Admin <Admin@DESKTOP-TS0M18F>
2024-04-22 10:56:23 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace день_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nextbutton = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Nextbutton
+            // 
+            this.Nextbutton.Location = new System.Drawing.Point(234, 366);
+            this.Nextbutton.Name = "Nextbutton";
+            this.Nextbutton.Size = new System.Drawing.Size(127, 57);
+            this.Nextbutton.TabIndex = 1;
+            this.Nextbutton.Text = "Ответить";
+            this.Nextbutton.UseVisualStyleBackColor = true;
+            this.Nextbutton.Click += new System.EventHandler(this.Nextbutton_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(27, 40);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(27, 66);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(27, 92);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(103, 20);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(27, 118);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(103, 20);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Location = new System.Drawing.Point(12, 126);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(641, 180);
+            this.panel1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(641, 73);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(593, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(535, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Баллы:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 440);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 465);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Nextbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Nextbutton;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace день_13
+{
+    public partial class Form1 : Form
+    {
+        string filepath = "C:\\Users\\Admin\\Desktop\\Практика КПИЯП\\Приложения по дням\\13 день\\questions.txt";
+        private string[] questions; // Массив для хранения вопросов
+        private int currentQuestionIndex = 0; // Индекс текущего вопроса
+        private int chlen = 0; // Индекс текущего вопроса
+        private int radio_index = 1; // Индекс radiobuttons
+        byte[] correctAnswers = new byte[] { 2, 1, 2, 0, 2, 0, 1, 0, 1, 0 };
+        private int score = 0; // Переменная для хранения количества правильных ответов
+        private Timer timer; // Таймер
+        private int remainingTime = 10; // Оставшееся время в секундах
+
+        public Form1()
+        {
+            InitializeComponent();
+            questions = File.ReadAllLines(filepath);
+            DisplayCurrentQuestion(); // Отображение первого вопроса
+            DisplayRadio();
+            StartTimer(); // Запуск таймера
+        }
+
+        private void StartTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 1000; // Интервал таймера - 1 секунда
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            remainingTime--;
+            if (remainingTime <= 0)
+            {
+                // По истечении времени останавливаем таймер и подсчитываем баллы
+                timer.Stop();
+                MessageBox.Show($"Время вышло! Вы набрали {score} баллов.");
+                // Блокировка кнопки "Next"
+                label1.Text = score.ToString();
+                label3.Text = "Время вышло!!!!!!!";
+                Nextbutton.Enabled = false;
+            }
+            else
+            {
+                // Обновление текста лейбла с оставшимся временем
+                label3.Text = $"Оставшееся время: {remainingTime} сек.";
+            }
+        }
+
+        private void DisplayCurrentQuestion()
+        {
+            if (currentQuestionIndex < 10)
+            {
+                // Отображение текущего вопроса и его номера
+                textBox1.Text = $"Вопрос {currentQuestionIndex + 1}:\r\n{questions[chlen]}";
+                chlen += 6;
+            }
+            else
+            {
+                // Все вопросы закончились, выводим сообщение
+                textBox1.Text = "Тест завершен!";
+                label1.Text = score.ToString();
+            }
+        }
+
+        private int CheckAnswer(byte correctAnswer, byte selectedAnswer)
+        {
+            if (correctAnswer == selectedAnswer)
+            {
+                return 1; // Возвращаем 1 балл за правильный ответ
+            }
+            else
+            {
+                return 0; // Возвращаем 0 баллов за неправильный ответ
+            }
+        }
+
+        private void DisplayRadio()
+        {
+            if (radio_index < questions.Length)
+            {
+                radioButton1.Text = $"{questions[radio_index]}";
+                radioButton2.Text = $"{questions[radio_index + 1]}";
+                radioButton3.Text = $"{questions[radio_index + 2]}";
+                radioButton4.Text = $"{questions[radio_index + 3]}";
+                radio_index += 6;
+            }
+            else
+            {
+                radioButton1.Text = "";
+                radioButton2.Text = "";
+                radioButton3.Text = "";
+                radioButton4.Text = "";
+            }
+        }
+
+        private void Nextbutton_Click(object sender, EventArgs e)
+        {
+            byte selectedAnswer;
+            if (radioButton1.Checked)
+            {
+                selectedAnswer = 0;
+            }
+            else if (radioButton2.Checked)
+            {
+                selectedAnswer = 1;
+            }
+            else if (radioButton3.Checked)
+            {
+                selectedAnswer = 2;
+            }
+            else if (radioButton4.Checked)
+            {
+                selectedAnswer = 3;
+            }
+            else
+            {
+                // Ни один ответ не выбран
+                MessageBox.Show("Пожалуйста, выберите ответ");
+                return;
+            }
+            int scoreForQuestion = 0;
+            if (currentQuestionIndex < 10)
+            {
+                scoreForQuestion = CheckAnswer(correctAnswers[currentQuestionIndex], selectedAnswer);
+                score += scoreForQuestion;
+            }
+
+            // Выводим сообщение о правильности ответа
+            if (scoreForQuestion == 1)
+            {
+                MessageBox.Show("Правильно!");
+            }
+            else
+            {
+                MessageBox.Show("Неправильно!");
+            }
+
+            currentQuestionIndex++;
+            DisplayCurrentQuestion();
+            DisplayRadio();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
